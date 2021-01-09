27610f4c0fcfe762c2e1e1ef851ad8f890d8527e
Jack <jackmy35@gmail.com>
2021-01-09 13:23:56 +1300

    test scripts

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
-from distutils.core import setup
+from setuptools import setup
 import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 try:
-    README = open(os.path.join(here, 'README.txt')).read()
+    README = open(os.path.join(here, 'README.md')).read()
 except (IOError, OSError):
     README = ''
 
 setup(
   name = 'supervisor-discord',
   packages = ['supervisor-discord'],
-  version = '1',
+  version = '1.0.0',
   license='GNU GPL v3',
   description = 'Connect supervisor to discord via webhooks',
   long_description=README,
+  long_description_content_type='text/markdown',
   author = 'chaos_a',
   url = 'https://github.com/chaosay/supervisor-discord',
   download_url = 'https://github.com/chaosay/supervisor-discord/archive/1.0.tar.gz',    # I explain this later on
   keywords = ['supervisor', 'discord', 'alerts'],
+  scripts=['supervisor-discord/alertHandler.py'],
   install_requires=[
           'pyYaml',
           'ratelimit',
@@ -30,7 +32,7 @@ setup(
     'Topic :: System :: Boot',
     'Topic :: System :: Monitoring',
     'Topic :: System :: Systems Administration',
-    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
